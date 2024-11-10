0981af25c54987a95b44c400631334dae4f39c43
Wesley <wesleysantoscardoso2404@gmail.com>
2024-11-10 17:53:12 -0300

    Resolvido erros

--- view/service/service.js
+++ view/service/service.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signup')
 var btnColor = document.querySelector('.btnColor')
@@ -15,22 +14,4 @@ document.querySelector('#btnSignup')
     formSignin.style.left = "-450px"
     formSignup.style.left = "25px"
     btnColor.style.left = "110px"
-=======
-var formSignin = document.querySelector('#signin')
-var formSignup = document.querySelector('#signup')
-var btnColor = document.querySelector('.btnColor')
-
-document.querySelector('#btnSignin')
-    .addEventListener('click', () => {
-    formSignin.style.left = "25px"
-    formSignup.style.left = "450px"
-    btnColor.style.left = "0px"
-})
-
-document.querySelector('#btnSignup')
-    .addEventListener('click', () => {
-    formSignin.style.left = "-450px"
-    formSignup.style.left = "25px"
-    btnColor.style.left = "110px"
->>>>>>> 4c6541f7347c209d3564b4c5d9905af650fb640d
-})
+    })
